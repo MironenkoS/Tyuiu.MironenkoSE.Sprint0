74b49080c0c75e545b7e576357b01c255967b980
664 <stephencurrytopone@gmail.com>
2024-09-11 17:37:33 +0500

    Task1

---
+++ Tyuiu.MironenkoSE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MironenkoSE.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string name = "Sergey";
+            int age = 18;
+            Console.WriteLine("Зравствуйте меня зовут " + name  + " мне " + age + " лет! ");
+            Console.ReadKey();
+        
+        }
+    }
+}
