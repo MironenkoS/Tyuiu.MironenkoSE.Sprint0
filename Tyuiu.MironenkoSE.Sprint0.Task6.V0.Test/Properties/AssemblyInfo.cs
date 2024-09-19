e1dfd602ae71dacb4c4cd9797b240bd015c59875
664 <stephencurrytopone@gmail.com>
2024-09-19 19:34:14 +0500

    Task6

---
+++ Tyuiu.MironenkoSE.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MironenkoSE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MironenkoSE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fbcba367-2431-4b28-bc50-64f88f719545")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MironenkoSE.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MironenkoSE.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.MironenkoSE.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = { 1, 2, 3, 4, 5};
+            Console.Write("Сумма элементов массива = "  + DataService.AdditionArray(numsArray));
+
+            Console.Write("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+
+            Console.Write("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
